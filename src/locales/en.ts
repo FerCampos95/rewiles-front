162406367cc65987e9c2664629cc001c23c3edcb
FerCamposDev <fernando.developments@gmail.com>
2023-02-19 22:08:17 -0300

    multilang support

---
+++ src/locales/en.ts
@@ -0,0 +1,13 @@
+import { IHomeWordings } from '.';
+
+export const en: IHomeWordings = {
+	sections: {
+		home: 'Home',
+		products: 'Products',
+		contact: 'Contact'
+	},
+	home: {
+		title: 'Experience the Magic of Innovative App Development Solutions',
+		desc: 'This is a long description'
+	}
+};
---
+++ src/locales/index.ts
@@ -0,0 +1,14 @@
+export { en } from './en';
+export { es } from './es';
+
+export interface IHomeWordings {
+  sections: {
+    home: string;
+    products: string;
+    contact: string;
+  },
+  home: {
+    title: string;
+    desc: string;
+  }
+}
