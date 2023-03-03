9e7b562917788a90d8bd5db2a7d116211c81e6d1
fercamposdev <fernando.developments@gmail.com>
2023-03-03 15:37:45 -0300

    form

---
+++ TODO.js
@@ -0,0 +1,9 @@
+// separar navegacion en desktop
+// armar form
+// funcionalidad form
+// colores en textos
+
+
+// MOBILE
+// acomodar imagenes
+// sincronizar transiciones
--- src/components/LadingSections/Contact.tsx
+++ src/components/LadingSections/Contact.tsx
@@ -1,5 +1,5 @@
 import DesktopBlob from '@/assets/blobs/DesktopBlob';
-import { Grid, Stack, Typography } from '@mui/material';
+import { Button, Grid, Stack, TextField, Typography } from '@mui/material';
 
 const Contact = () => {
 	return (
@@ -14,16 +14,47 @@ const Contact = () => {
 			p={4}
 		>
 			<DesktopBlob right='0' bottom='0' width="50vw" style={{ position: 'absolute' }}/>
-			<Grid container justifyContent="center">
-				<Stack maxWidth={630} gap={4} textAlign="justify" color="#282728">
-					<Typography variant="h1" gutterBottom textAlign="center" component={'h2'}>
-            Contact
-					</Typography>
+			<Grid container justifyContent="center" color="#282728">
+				<Grid container maxWidth={630} spacing={4}>
+					<Grid item xs={12}>
+						<Typography variant="h1" gutterBottom textAlign="center" component={'h2'}>
+              Contact
+						</Typography>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<TextField
+							fullWidth
+							label="Name"
+						/>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<TextField
+							fullWidth
+							label="Email"
+							type="email"
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							fullWidth
+							label="Subject"
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							fullWidth
+							label="Description"
+							rows={5}
+							helperText="Please enter the details of your request. A member of our support staff will respond as soon as possible"
+						/>
+					</Grid>
 
-					<Typography variant="h6">
-            FORM
-					</Typography>
-				</Stack>
+					<Grid container justifyContent="center" mt={18}>
+						<Button type='submit'>
+						Submit
+						</Button>
+					</Grid>
+				</Grid>
 			</Grid>
 		</Stack>
 	);
