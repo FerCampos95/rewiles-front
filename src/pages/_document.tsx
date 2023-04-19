574487baef42e411fdfac2266bcf4322398fa98c
FerCampos95 <55367115+FerCampos95@users.noreply.github.com>
2023-04-19 19:30:59 -0300

    Revert "1.0.2 (#3)"
    This reverts commit b3361e346ce841588ab3e60f919fbdef7efa47fe.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,52 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import createEmotionCache from '@/utils/emotionCache';
-import createEmotionServer from '@emotion/server/create-instance';
-import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
-	render() {
-		return (
-			<Html lang="en">
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+export default function Document() {
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
-
-MyDocument.getInitialProps = async (ctx) => {
-	const originalRenderPage = ctx.renderPage;
-
-	const cache = createEmotionCache();
-	const { extractCriticalToChunks } = createEmotionServer(cache);
-
-	ctx.renderPage = () =>
-		originalRenderPage({
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			enhanceApp: (App: any) => function EnhanceApp(props) {
-				return <App emotionCache={cache} {...props} />;
-			},
-		});
-
-	const initialProps = await Document.getInitialProps(ctx);
-	// See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
-	const emotionStyles = extractCriticalToChunks(initialProps.html);
-	const emotionStyleTags = emotionStyles.styles.map((style) => (
-		<style
-			data-emotion={`${style.key} ${style.ids.join(' ')}`}
-			key={style.key}
-			dangerouslySetInnerHTML={{ __html: style.css }}
-		/>
-	));
-
-	return {
-		...initialProps,
-		styles: [
-			...React.Children.toArray(initialProps.styles),
-			...emotionStyleTags,
-		]
-	};
-};
